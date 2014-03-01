bad6d015a3ff0456d35ecf5ff0204b0d7e7f6fdf
han seong young <syhan@nextree.co.kr>
2014-03-01 12:06:40 +0900

    socket write test

--- kr.namoosori.naite.ri.plugin.teacher/test/kr/namoosori/naite/ri/plugin/teacher/network/socket/SocketClient.java
+++ kr.namoosori.naite.ri.plugin.teacher/test/kr/namoosori/naite/ri/plugin/teacher/network/socket/SocketClient.java
@@ -1,11 +1,31 @@
 package kr.namoosori.naite.ri.plugin.teacher.network.socket;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SocketClient {
-	public static void main(String[] args) throws Exception {
-		Socket socket = new Socket("10.0.1.83", 4449);
+	private Socket socket;
+	
+	public void send(String message) throws UnknownHostException, IOException {
+		socket = new Socket("10.0.1.83", 4449);
 		System.out.println("connect");
+		write(socket.getOutputStream(), message);
+	}
+	
+	private void write(OutputStream outputStream, String message) throws IOException {
+		OutputStreamWriter out = new OutputStreamWriter(outputStream);
+		BufferedWriter bw = new BufferedWriter(out);
+		bw.write(message);
+		System.out.println("write:"+message);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SocketClient client = new SocketClient();
+		client.send("hello teacher");
 	}
 
 }
--- kr.namoosori.naite.ri.plugin.teacher/test/kr/namoosori/naite/ri/plugin/teacher/network/socket/SocketServer.java
+++ kr.namoosori.naite.ri.plugin.teacher/test/kr/namoosori/naite/ri/plugin/teacher/network/socket/SocketServer.java
@@ -1,25 +1,44 @@
 package kr.namoosori.naite.ri.plugin.teacher.network.socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServer extends Thread {
 	//
+	private ServerSocket serverSocket;
+	
 	@Override
 	public void run() {
 		try {
-			ServerSocket serverSocket = new ServerSocket(4449);
-			Socket socket = serverSocket.accept();
-			System.out.println("accept! -->" +socket.getLocalSocketAddress());
-			socket.close();
-			serverSocket.close();
-			System.out.println("end!");
+			serverSocket = new ServerSocket(4449);
+			while (true) {
+				System.out.println("server wait...");
+				Socket socket = serverSocket.accept();
+				System.out.println("accept! -->" +socket.getInetAddress());
+				String message = receive(socket.getInputStream());
+				System.out.println("receive message:"+message);
+				socket.close();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	private String receive(InputStream inputStream) throws IOException {
+		InputStreamReader reader = new InputStreamReader(inputStream);
+		BufferedReader br = new BufferedReader(reader);
+		String read = null;
+		StringBuffer sb = new StringBuffer();
+		while ((read = br.readLine()) != null) {
+			sb.append(read);
+		}
+		return sb.toString();
+	}
+
 	public static void main(String[] args) {
 		SocketServer server = new SocketServer();
 		server.start();
